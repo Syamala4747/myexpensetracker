1242aa64fb63049c225e9f9c31426be3162758eb
CHINTAPALLI NAGA SYAMALA <syamala_csd@srkrec.edu.in>
2025-07-06 23:47:07 +0530

    Update apiPaths.js

--- frontend/expense-tracker/src/utils/apiPaths.js
+++ frontend/expense-tracker/src/utils/apiPaths.js
@@ -1,4 +1,4 @@
-export const BASE_URL = "http://localhost:3000";                 
+export const BASE_URL = "https://myexpensetracker-backend.onrender.com";                 
 
 // utils/apiPaths.js
 export const API_PATHS = {
