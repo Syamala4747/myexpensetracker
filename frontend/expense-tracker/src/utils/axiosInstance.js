b20a9e185875d4e1539796d765ae272e971c2142
CHINTAPALLI NAGA SYAMALA <syamala_csd@srkrec.edu.in>
2025-07-07 00:31:23 +0530

    Update axiosInstance.js

--- frontend/expense-tracker/src/utils/axiosInstance.js
+++ frontend/expense-tracker/src/utils/axiosInstance.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { BASE_URL } from "./apiPaths";
 
 const axiosInstance = axios.create({
-  baseURL: "https://myexpensetracker-backend.onrender.com",
+  baseURL: import.meta.env.VITE_API_URL,
   timeout: 10000,
   headers: {
     "Content-Type": "application/json",
